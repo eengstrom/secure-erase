3b4e9e628f6788a5293b252f114e2b0b36c85761
Eric Engstrom <engstrom@sift.net>
2023-06-09 09:29:32 -0500

    rework UX/feedback, providing more info before wipe confirmation

--- ata-secure-erase.sh
+++ ata-secure-erase.sh
@@ -15,15 +15,15 @@
 
 # Linted with shellcheck: https://github.com/koalaman/shellcheck
 
-# References: 
+# References:
 # T13/1699-D, AT Attachment 8 - ATA/ATAPI Command Set (ATA8-ACS)
 # http://www.t13.org/documents/UploadedDocuments/docs2007/D1699r4a-ATA8-ACS.pdf
 
 # DISCLAIMER
-# Whilst care has been taken to thoroughly test this script using a variety of 
+# Whilst care has been taken to thoroughly test this script using a variety of
 # cases, # as with all Linux/UNIX-like affairs, it may still fail in mysterious
 # circumstances. In particular, ERASE UNIT may fail to erase some SSD drives:
-# M. Wei, L. M. Grupp, F. M. Spada, and S. Swanson, 'Reliably Erasing Data 
+# M. Wei, L. M. Grupp, F. M. Spada, and S. Swanson, 'Reliably Erasing Data
 # from Flash-Based Solid State Drives'
 # https://www.usenix.org/legacy/event/fast11/tech/full_papers/Wei.pdf
 
@@ -65,18 +65,18 @@ list_available_disks() {
         fi
     done
     if [[ ${#eligible_disks[@]} -gt 0 ]]; then
-        echo "Available disks for secure erase are:" 
+        echo "Available disks for secure erase are:"
         for eligible_disk in "${eligible_disks[@]}"; do
             echo "${eligible_disk}"
         done
     else
-        echo >&2 "No supported disks found for secure erase" 
+        echo >&2 "No supported disks found for secure erase"
     fi
 }
 
 is_block_device() {
     local file="$1"
-    if [[ -b ${file} ]]; then 
+    if [[ -b ${file} ]]; then
         true
     else
         false
@@ -87,11 +87,11 @@ ata_erase_support() {
     local disk="$1"
     local -a hdparm_identify_result
     local identify_index
-    # If SECURITY ERASE UNIT is supported, hdparm -I (identify) output should 
+    # If SECURITY ERASE UNIT is supported, hdparm -I (identify) output should
     # match "Security:" with "supported" on either the first or second
     # subsequent line:
     # https://salsa.debian.org/debian/hdparm/blob/master/identify.c
-    readarray -t hdparm_identify_result < <( hdparm -I "${disk}" 2>/dev/null ) 
+    readarray -t hdparm_identify_result < <( hdparm -I "${disk}" 2>/dev/null )
     # Could probably have done this with grep -c instead. Oh, well.
     for identify_index in "${!hdparm_identify_result[@]}"; do
         if [[ "${hdparm_identify_result[$identify_index]}" =~ ^Security: ]]; then
@@ -133,14 +133,23 @@ is_password_set () {
     fi
 }
 
+show_disk_partitions () {
+    local ata_disk="$1"
+    lsblk -o 'NAME,TYPE,SIZE,FSTYPE,FSVER,LABEL,MOUNTPOINT' "${ata_disk}" \
+        | sed 's/^[\t ]*/  /'
+}
+
 estimate_erase_time () {
     local ata_disk="$1"
-    hdparm -I "${ata_disk}" | awk '/for SECURITY ERASE UNIT/'
+    echo "ESTIMATED time to erase:"
+    hdparm -I "${ata_disk}" \
+        | awk -F. '/for SECURITY ERASE UNIT/ {print $1 "\n" $2}' \
+        | sed 's/^[\t ]*/  /'
 }
 
 erase_disk() {
     local ata_disk="$1"
-    hdparm --user-master u --security-erase "${DISK_PASSWORD}" "$ata_disk" >/dev/null 
+    hdparm --user-master u --security-erase "${DISK_PASSWORD}" "$ata_disk" >/dev/null
 }
 
 main() {
@@ -149,7 +158,6 @@ main() {
     local kernel_release
     local kernel_version
     local kernel_version_numeric
-    local ata_disk="$1"
     local force=false
     local -l user_choice # Force lower case
 
@@ -181,7 +189,7 @@ main() {
         exit 1
     fi
 
-    # POSIX-compliant method for checking a command is available 
+    # POSIX-compliant method for checking a command is available
     local required_command
     for required_command in ${REQUIRED_COMMANDS}
     do
@@ -190,11 +198,6 @@ main() {
             exit 1
         fi
     done
-
-    if [[ ${ARGC[0]} -lt 1 || ${ARGC[0]} -gt 2 ]]; then
-        show_usage
-        exit 1
-    fi
 
     while getopts ":f:hl" option
     do
@@ -209,7 +212,6 @@ main() {
             ;;
         f)
             force=true
-            ata_disk="$2"
             ;;
         *)
             show_usage
@@ -218,53 +220,64 @@ main() {
         esac
     done
 
-    # Check the specifed device exists and is a block device
-    if ! is_block_device "${ata_disk}"; then 
-        echo >&2 "Error: No such block device ${ata_disk}, use ${SCRIPT_NAME} -l to list available disks"
-        exit 1 
+    # remaining args should start with disk to erase
+    local ata_disk="$1"
+    if [[ -z "${ata_disk}" ]]; then
+        show_usage
+        exit 1
     fi
 
-    if ! ata_erase_support "${ata_disk}"; then 
+    # Check the specifed device exists and is a block device
+    if ! is_block_device "${ata_disk}"; then
+        echo >&2 "Error: No such block device ${ata_disk}, use ${SCRIPT_NAME} -l to list available disks"
+        exit 1
+    fi
+
+    if ! ata_erase_support "${ata_disk}"; then
         echo >&2 "Error: ATA SECURITY ERASE UNIT unsupported on ${ata_disk}, use ${SCRIPT_NAME} -l to list available disks"
-        exit 1 
+        exit 1
     fi
 
     # Check for frozen state
     if ! is_unfrozen "${ata_disk}"; then
-        echo >&2 "Error: Disk ${ata_disk} security state is frozen, check https://ata.wiki.kernel.org/index.php/ATA_Secure_Erase for options"
+        echo >&2 "Error: Disk ${ata_disk} security state is frozen; c.f. https://ata.wiki.kernel.org/index.php/ATA_Secure_Erase for options"
         exit 1
     fi
 
-    if [[ "${force}" == false ]]; then
-        echo "[5m[1;31mWARNING: this procedure will erase all data on ${ata_disk} beyond recovery.[0m" 
-        echo -n "Continue [Y/N]?"
-        read -r user_choice
+    if ! ${force}; then
+        echo "[5m[1;31mWARNING: about to erase all data on ${ata_disk} beyond recovery:[0m"
+        show_disk_partitions "${ata_disk}"
+        estimate_erase_time "${ata_disk}"
+        read -p "Continue [y/N]? " -r user_choice
     fi
 
-    if [[ "${force}" == true || "${user_choice}" == "y" ]]; then
-        echo "Attempting to set user password and enable secure erase..."
-        set_password "${ata_disk}"
-    else
+    if ! ${force} && [[ "${user_choice}" != [yY] ]]; then
         echo "Secure erase operation cancelled"
         exit 0
     fi
 
-    if is_password_set "${ata_disk}"; then
+    echo -n "Attempting to set user password and enable secure erase..."
+    set_password "${ata_disk}"
+    if ! is_password_set "${ata_disk}"; then
+        echo " password set."
+    else
+        echo
         echo >&2 "Error setting user password on ${ata_disk}"
         exit 1
-    else
-        echo "User password set, attempting secure erase for ${ata_disk}";
-        estimate_erase_time "${ata_disk}"
-        erase_disk "${ata_disk}"
-        # Sucessful erase should reset "enabled" value to "not"
-        if ! is_password_set "${ata_disk}"; then
-            echo "Secure erase was successful for ${ata_disk}";
-            exit 0
-        else
-            echo >&2 "Error performing secure erase on ${ata_disk}"
-            exit 1
-        fi;
     fi
+
+    echo "Initiating secure erase for ${ata_disk}"
+    erase_disk "${ata_disk}"
+    # re-read partition table
+    partprobe  "${ata_disk}"
+    # Sucessful erase should reset "enabled" value to "not"
+    if ! is_password_set "${ata_disk}"; then
+        echo "Secure erase was successful for ${ata_disk}";
+        exit 0
+    else
+        echo >&2 "Error performing secure erase on ${ata_disk}"
+        exit 1
+    fi;
 }
 
-main "${ARGV[@]}" 
+main "${ARGV[@]}"
